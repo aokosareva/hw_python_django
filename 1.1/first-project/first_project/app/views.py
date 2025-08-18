d79601941eb124d234e4fef9d2c4069bc545b9a8
aokosareva <143634869+aokosareva@users.noreply.github.com>
2025-08-18 17:38:42 +0300

    Feature/first project (#1)
    * 1.1 - frist_project homework
    
    * 1.1 - frist_project homework
    
    * refactor workdir
    
    ---------
    
    Co-authored-by: Anastasia Kosareva <aokosareva@gmail.com>

---
+++ 1.1/first-project/first_project/app/views.py
@@ -0,0 +1,36 @@
+import datetime
+import os
+
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+
+    # context и параметры render менять не нужно
+    # подбробнее о них мы поговорим на следующих лекциях
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    time_zone = datetime.timezone(datetime.timedelta(hours=3), name='Msk')
+    current_time = datetime.datetime.now(tz=time_zone).time()
+    msg = f'Текущее время: {current_time.strftime('%H:%M:%S')}'
+
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    workdir_path = os.getcwd()
+    workdir = os.listdir(workdir_path)
+    
+    return HttpResponse(f"Содержимое рабочей директории [{workdir_path}]:<br> -- {",<br> -- ".join(workdir)}")
