d1e30218c292fe538fd951e8c1a3273ad87ffef3
Anastasia Kosareva <aokosareva@gmail.com>
2025-08-22 16:21:41 +0300

    cars

--- 2.1/orm_shop/main/admin.py
+++ 2.1/orm_shop/main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from main.models import Client, Car, Sale
+
 # зарегистрируйте необходимые модели
+
+
+admin.site.register(Client)
+admin.site.register(Car)
+admin.site.register(Sale)
