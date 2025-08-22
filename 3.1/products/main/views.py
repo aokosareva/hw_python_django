0fe7c6df4e7005237a81e2328a710e6075a9b5d9
aokosareva <143634869+aokosareva@users.noreply.github.com>
2025-08-22 21:30:32 +0300

    drf. products (#6)
    Co-authored-by: Anastasia Kosareva <aokosareva@gmail.com>

--- 3.1/products/main/views.py
+++ 3.1/products/main/views.py
@@ -1,32 +1,48 @@
-from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from main.serializers import ReviewSerializer, ProductListSerializer, ProductDetailsSerializer
+from main.models import Product, Review
+from main.serializers import ProductListSerializer, ProductDetailsSerializer, ReviewSerializer
 
 
 @api_view(['GET'])
 def products_list_view(request):
-    """реализуйте получение всех товаров из БД
-    реализуйте сериализацию полученных данных
-    отдайте отсериализованные данные в Response"""
-    pass
+    try:
+        products = Product.objects.all()
+        serializer = ProductListSerializer(products, many=True)
+
+        return Response(serializer.data)
+    except Exception as e:
+        return Response({"message": str(e)}, status=500)
 
 
 class ProductDetailsView(APIView):
     def get(self, request, product_id):
-        """реализуйте получение товара по id, если его нет, то выдайте 404
-        реализуйте сериализацию полученных данных
-        отдайте отсериализованные данные в Response"""
-        pass
+        try:
+            product = Product.objects.get(id=product_id)
+            serializer = ProductDetailsSerializer(product)
+
+            return Response(serializer.data)
+        except ObjectDoesNotExist as e:
+            return Response({"message": str(e)}, status=404)
+        except Exception as e:
+            return Response({"message": str(e)}, status=500)
 
 
 # доп задание:
 class ProductFilteredReviews(APIView):
     def get(self, request, product_id):
-        """обработайте значение параметра mark и
-        реализуйте получение отзывов по конкретному товару с определённой оценкой
-        реализуйте сериализацию полученных данных
-        отдайте отсериализованные данные в Response"""
-        pass
+        mark = request.GET.get("mark", "")
+
+        if len(mark) == 0:
+            reviews = Review.objects.filter(product=product_id)
+        elif mark.isdigit():
+            reviews = Review.objects.filter(product=product_id, mark=mark)
+        else:
+            return Response({"message": "Invalid parameter \"mark\"."})
+
+        serializer = ReviewSerializer(reviews, many=True)
+
+        return Response(serializer.data)
